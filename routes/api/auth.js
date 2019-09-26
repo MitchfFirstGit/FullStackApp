d6d8ac57a2b351e1e1fca9821a8cb4a3f57fb235
Artem.Chaikovskyi <Artem.Chaikovskyi@innovecs.com>
2019-09-26 16:18:08 +0300

    Create Profile model

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,6 +2,10 @@ const express = require("express");
 const router = express.Router();
 const auth = require('../../middleware/auth');
 const User = require('../../models/User');
+const { check, validationResult } = require('express-validator');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const bcrypt = require('bcryptjs');
 
 router.get('/', auth, async (req, res) => {
     try {
@@ -12,5 +16,55 @@ router.get('/', auth, async (req, res) => {
         res.send(500).send('Server error');
     } 
 });
+
+router.post('/', [
+    check('email', 'Please includes valid email').isEmail(),
+    check(
+        'password',
+        'Password is required'
+    ).exists()
+],
+    async (req, res) => {
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        const { email, password } = req.body;
+
+        try {
+            let user = await User.findOne({ email });
+
+            if (!user) {
+                return res.status(400).json({ errors: [{ msg: 'Invalid credentisls' }] });
+            }
+
+            const isMatch = await bcrypt.compare(password, user.password);
+
+            if(!isMatch) {
+                return res.status(400).json({ errors: [{ msg: 'Invalid credentisls' }] });
+            }
+            
+            const payload = {
+                user: {
+                    id: user.id,
+                }
+            }
+
+            jwt.sign(
+                payload,
+                config.get('jwtSecret'),
+                (err, token) => {
+                    if(err) throw err;
+                    res.json({ token });
+                }
+            );
+
+        } catch (err) {
+            console.log('Server error');
+            res.status(500).send('Server error');
+        }
+    });
 
 module.exports = router;
