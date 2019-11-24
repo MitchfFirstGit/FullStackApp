879f5ff135ace3f4c47590dc305a8f0a553ce848
Mitch <artfixmitch@gmail.com>
2019-11-24 18:28:18 +0200

    Display aducation and experience

--- client/src/components/Profile/Profile.js
+++ client/src/components/Profile/Profile.js
@@ -6,6 +6,8 @@ import Spinner from '../layout/Spinner';
 import { getProfileById } from '../../actions/profile';
 import ProfileTop from './components/ProfileTop';
 import ProfileAbout from './components/ProfileAbout';
+import ProfileExperience from './components/ProfileExperience';
+import ProfileEducation from './components/ProfileEducation';
 
 const Profile = ({
   getProfileById,
@@ -38,6 +40,38 @@ const Profile = ({
             <div className='profile-grid my-1'>
               <ProfileTop profile={profile} />
               <ProfileAbout profile={profile} />
+
+              <div className='profile-exp bg-white p-2'>
+                <h2 className='text-primary'>Experience</h2>
+                {profile.experience.length > 0 ? (
+                  <>
+                    {profile.experience.map(experience => (
+                      <ProfileExperience
+                        key={experience._id}
+                        experience={experience}
+                      />
+                    ))}
+                  </>
+                ) : (
+                    <h4>No experience</h4>
+                  )}
+              </div>
+
+              <div className='profile-edu bg-white p-2'>
+                <h2 className='text-primary'>Education</h2>
+                {profile.education.length > 0 ? (
+                  <>
+                    {profile.education.map(education => (
+                      <ProfileEducation
+                        key={education._id}
+                        education={education}
+                      />
+                    ))}
+                  </>
+                ) : (
+                    <h4>No education </h4>
+                  )}
+              </div>
             </div>
           </>
         )}
